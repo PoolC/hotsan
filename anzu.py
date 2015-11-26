e08b849a639406df18ee03c875f70210ff6a4129
sapphire <se.imas.kr@gmail.com>
2015-11-26 11:16:02 +0900

    use re to match commands

--- anzu.py
+++ anzu.py
@@ -13,27 +13,29 @@ def process(r):
                 yield from bot.post(message['channel'], '이거 놔라 이 퇴근도 못하는 놈이')
             elif message.get('text', '').startswith('안즈쨩 카와이'):
                 yield from bot.post(message['channel'], "뭐... 뭐라는거야\n기억해 [key]/[val], 알려줘 [key] 만 시키라구")
-            elif message.get('text', '').startswith('안즈쨩 기억해') or message.get('text', '').startswith('안즈 기억'):
-                matched = remember_re.match(message.get('text', ''))
-                key = matched.group(1).strip()
-                val = matched.group(2).strip()
-                if not key or not val:
-                    yield from bot.post(message['channel'], '에...?')
-                elif '안즈쨩 알려줘' in val:
-                    yield from bot.post(message['channel'], '에... 귀찮아')
-                else:
-                    r.set(key, val)
-                    yield from bot.post(message['channel'], '에... 귀찮지만 기억했어')
-            elif message.get('text', '').startswith('안즈쨩 알려줘') or message.get('text', '').startswith('안즈 알려줘'):
-                key = tell_re.match(message.get('text', '')).group(1).strip()
-                val = r.get(key)
-                if not val:
-                    val = '그런거 몰라'
-                if isinstance(val, bytes):
-                    val = val.decode('utf-8')
-                yield from bot.post(message['channel'], val)
             elif message.get('text', '') == '안즈쨩 뭐해?':
                 yield from bot.post(message['channel'], '숨셔')
+            else:
+                matched = remember_re.match(message.get('text', ''))
+                if matched:
+                    key = matched.group(1).strip()
+                    val = matched.group(2).strip()
+                    if not key or not val:
+                        yield from bot.post(message['channel'], '에...?')
+                    elif '안즈쨩 알려줘' in val:
+                        yield from bot.post(message['channel'], '에... 귀찮아')
+                    else:
+                        r.set(key, val)
+                        yield from bot.post(message['channel'], '에... 귀찮지만 기억했어')
+                matched = tell_re.match(message.get('text', ''))
+                if matched:
+                    key = tell_re.match(message.get('text', '')).group(1).strip()
+                    val = r.get(key)
+                    if not val:
+                        val = '그런거 몰라'
+                    if isinstance(val, bytes):
+                        val = val.decode('utf-8')
+                    yield from bot.post(message['channel'], val)
         except Exception as e:
             logging.error("Anzu : %s", repr(e))
     return impl
