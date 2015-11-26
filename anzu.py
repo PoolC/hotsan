661b0bbde4d15241fdce4c50ba05a473c5078be3
sapphire <se.imas.kr@gmail.com>
2015-11-26 11:02:17 +0900

    simple commands for anzu

--- anzu.py
+++ anzu.py
@@ -1,5 +1,9 @@
 import asyncio
 import logging
+import re
+
+remember_re = re.compile('^안즈쨩? 기억해? ([^/]+)/(.+)$')
+tell_re = re.compile('^안즈쨩? 알려줘 (.+)$')
 
 def process(r):
     @asyncio.coroutine
@@ -9,10 +13,10 @@ def process(r):
                 yield from bot.post(message['channel'], '이거 놔라 이 퇴근도 못하는 놈이')
             elif message.get('text', '').startswith('안즈쨩 카와이'):
                 yield from bot.post(message['channel'], "뭐... 뭐라는거야\n기억해 [key]/[val], 알려줘 [key] 만 시키라구")
-            elif message.get('text', '').startswith('안즈쨩 기억해'):
-                key,val = message['text'][8:].split('/',1)
-                key = key.strip()
-                val = val.strip()
+            elif message.get('text', '').startswith('안즈쨩 기억해') or message.get('text', '').startswith('안즈 기억'):
+                matched = remember_re.match(message.get('text', ''))
+                key = matched.group(1).strip()
+                val = matched.group(2).strip()
                 if not key or not val:
                     yield from bot.post(message['channel'], '에...?')
                 elif '안즈쨩 알려줘' in val:
@@ -20,8 +24,8 @@ def process(r):
                 else:
                     r.set(key, val)
                     yield from bot.post(message['channel'], '에... 귀찮지만 기억했어')
-            elif message.get('text', '').startswith('안즈쨩 알려줘'):
-                key = message['text'][8:].strip()
+            elif message.get('text', '').startswith('안즈쨩 알려줘') or message.get('text', '').startswith('안즈 알려줘'):
+                key = tell_re.match(message.get('text', '')).group(1).strip()
                 val = r.get(key)
                 if not val:
                     val = '그런거 몰라'
